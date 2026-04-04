8029d8c84ccf4b397a4526d5f37f5b6c62318f50
Jairus <bg.jairtom098@gmai.com>
2026-04-04 09:47:17 +0300

    add commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const burgers = ['Hamburger', 'Cheeseburger']
 let featuredDrink = 'Strawberry Milkshake'
 
-function addBurger(){
+function addBurger() {
     const newBurger = 'Flatburger'
     burgers.push(newBurger)
 }
-if(true){
+
+if (true) {
     const anotherNewBurger = 'Maple Bacon Burger'
     burgers.push(anotherNewBurger)
 }
-addBurger()
-function changeFeaturedDrink(){
+
+function changeFeaturedDrink() {
     featuredDrink = 'The JavaShake'
 }
-changeFeaturedDrink()
+
 console.log(burgers)
 console.log(featuredDrink)
